b157073a06b3936c92407dd073e62d00fbda5353
Mbk15 <mubarakmuhammadjamiu@gmail.com>
2022-07-26 18:37:07 +0100

    Javscript promise

---
+++ Advance_HTML_and_CSS/JS_Concepts/Promise_Async-Await/main.js
@@ -0,0 +1,81 @@
+function callMbk(arr, addInput) {
+  arr.forEach((array) => console.log(array));
+  addInput(arr);
+}
+
+function sumArr(arr) {
+  let total = 0;
+  for (let index = 0; index < arr.length; index++) {
+    total += arr[index];
+    console.log(total);
+  }
+  return total;
+}
+callMbk([1, 2, 3, 5, 6, 7], sumArr);
+
+const mbkPromise = new Promise((resolve, reject) => {
+  const error = false;
+
+  if (!error) {
+    resolve("mbk don Promise sey e go resolve am");
+  } else {
+    reject("wetin you expect , he no keep hin promise so e reject am");
+  }
+});
+
+// console.log(mbkPromise)
+
+mbkPromise
+  .then((value) => {
+    return value + 1;
+  })
+  .then((newvalue) => {
+    console.log(newvalue);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const mbkNextPromise = new Promise((res, rej) => {
+  setTimeout(function () {
+    res("next promise balling ");
+  }, 3000);
+});
+
+mbkNextPromise.then((value) => {
+  console.log(value);
+});
+
+mbkPromise.then((value) => {
+  console.log(value);
+});
+const url = "https://jsonplaceholder.typicode.com/users";
+
+const users = fetch(url);
+
+console.log(users); // return a pending state
+
+users
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
+
+fetch("https://api.github.com/users")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+// await fetch(url, {
+//   mode: "no-cors",
+// }).then((response) => {
+//   console.log(response);
+// });
